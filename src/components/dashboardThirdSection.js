18f57f6f4188eaea3b43f79413b7799941d63920
rizwan <rizwanakramyt@gmail.com>
2024-01-30 20:43:42 +0500

    homepage line chart implemented

---
+++ src/components/dashboardThirdSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Linechart from "./linechart";
+
+export default function DashboardThirdSection() {
+    return(
+    <div className="third-section-container">
+        <Linechart />
+
+    </div>
+    );
+  
+}
